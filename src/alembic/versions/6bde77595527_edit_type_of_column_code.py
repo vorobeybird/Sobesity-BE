1aad19ca0dbee40f52c7f95c783c4eeb3c2ca6c0
devdeM1 <makd1232255@yandex.ru>
2023-04-12 20:52:56 +0300

    delete format file

---
+++ src/alembic/versions/6bde77595527_edit_type_of_column_code.py
@@ -0,0 +1,24 @@
+"""edit type of column code
+
+Revision ID: 6bde77595527
+Revises: 749a67e4589a
+Create Date: 2023-04-12 16:43:31.001579
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6bde77595527'
+down_revision = '749a67e4589a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.alter_column('question', 'code', type_=sa.Text())
+
+
+def downgrade() -> None:
+    op.alter_column('question', 'code', type_=sa.String())
