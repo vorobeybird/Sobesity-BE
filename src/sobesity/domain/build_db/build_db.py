4063ade2fbf3d073b93f06ecdc482a9a13435a73
devdeM1 <makd1232255@yandex.ru>
2023-04-20 12:00:24 +0300

    add if __name__ == __main__ on build_db file

--- src/sobesity/domain/build_db/build_db.py
+++ src/sobesity/domain/build_db/build_db.py
@@ -1,6 +1,5 @@
 import yaml
 import logging
-from pprint import pprint
 from sobesity.domain.entities import (
     SkillEntity,
     SkillFilterEnitity,
@@ -15,101 +14,101 @@ from sobesity.domain.entities import (
     TypeFilterEnitity,
     TypeId,
 )
-from sobesity.domain.services import (
-    SkillService,
-    QuestionService,
-    AnswerService,
-    TypeService,
-)
 from sobesity.webapp import init_dependency
 
-with open("sobesity/domain/build_db/data/fixture.yaml") as f:
-    templates = yaml.safe_load(f)
 
-conteiner = init_dependency()
-question_service = conteiner.services.question()
-skill_service = conteiner.services.skill()
-answer_service = conteiner.services.answer()
-type_service = conteiner.services.type()
-logger = logging.getLogger(__name__)
+def main():
+    with open("sobesity/domain/build_db/data/fixture.yaml") as f:
+        templates = yaml.safe_load(f)
 
-for skill in templates:
-    exist_skill = skill_service.get_list(SkillFilterEnitity(names=[skill]))
-    if not exist_skill:
-        skill_service.batch_create([SkillEntity(skill_id=None, name=skill)])
-        logger.info("Create skill")
+    conteiner = init_dependency()
+    question_service = conteiner.services.question()
+    skill_service = conteiner.services.skill()
+    answer_service = conteiner.services.answer()
+    type_service = conteiner.services.type()
+    logger = logging.getLogger(__name__)
 
-    created_skill = skill_service.get_list(SkillFilterEnitity(names=[skill]))
+    for skill in templates:
+        exist_skill = skill_service.get_list(SkillFilterEnitity(names=[skill]))
+        if not exist_skill:
+            skill_service.batch_create([SkillEntity(skill_id=None, name=skill)])
+            logger.info("Create skill")
 
-    questions_with_answers = templates.get(skill)["questions"]
+        created_skill = skill_service.get_list(SkillFilterEnitity(names=[skill]))
 
-    for question_with_answers in questions_with_answers:
-        question = question_with_answers["question"]
+        questions_with_answers = templates.get(skill)["questions"]
 
-        exist_question = question_service.get_list(
-            QuestionFilterEnitity(
-                questions=[question], skill_ids=[created_skill[0].skill_id]
-            )
-        )
+        for question_with_answers in questions_with_answers:
+            question = question_with_answers["question"]
 
-        if not exist_question:
-            type_of_question = question_with_answers["type"]
-
-            exist_type = type_service.get_list(
-                TypeFilterEnitity(names=[type_of_question])
-            )
-
-            if not exist_type:
-                type_service.batch_create(
-                    [TypeEntity(type_id=None, name=type_of_question)]
-                )
-                logger.info("Create type")
-
-            created_type = type_service.get_list(
-                TypeFilterEnitity(names=[type_of_question])
-            )
-
-            code = question_with_answers.get("code")
-            question_to_create = [
-                QuestionEntity(
-                    question_id=None,
-                    question=question,
-                    type_id=created_type[0].type_id,
-                    code=code,
-                    skill_id=created_skill[0].skill_id,
-                )
-            ]
-            question_service.batch_create(question_to_create)
-            logger.info("Create question")
-        answers_for_this_question = question_with_answers["answers"]
-        created_question = question_service.get_list(
-            QuestionFilterEnitity(
-                questions=[question], skill_ids=[created_skill[0].skill_id]
-            )
-        )
-
-        answers_to_create = []
-
-        for answer_with_right in answers_for_this_question:
-
-            answer = answer_with_right["answer"]
-            exist_answer = answer_service.get_list(
-                AnswerFilterEnitity(
-                    answers=[answer], question_ids=[created_question[0].question_id]
+            exist_question = question_service.get_list(
+                QuestionFilterEnitity(
+                    questions=[question], skill_ids=[created_skill[0].skill_id]
                 )
             )
-            if not exist_answer:
-                try:
-                    right = answer_with_right["right"]
-                except (KeyError):
-                    right = False
 
-                answers_to_create.append(
-                    AnswerEntity(
-                        answer_id=None,
-                        answer=answer,
-                        right=right,
-                        question_id=created_question[0].question_id,
+            if not exist_question:
+                type_of_question = question_with_answers["type"]
+
+                exist_type = type_service.get_list(
+                    TypeFilterEnitity(names=[type_of_question])
+                )
+
+                if not exist_type:
+                    type_service.batch_create(
+                        [TypeEntity(type_id=None, name=type_of_question)]
+                    )
+                    logger.info("Create type")
+
+                created_type = type_service.get_list(
+                    TypeFilterEnitity(names=[type_of_question])
+                )
+
+                code = question_with_answers.get("code")
+                question_to_create = [
+                    QuestionEntity(
+                        question_id=None,
+                        question=question,
+                        type_id=created_type[0].type_id,
+                        code=code,
+                        skill_id=created_skill[0].skill_id,
+                    )
+                ]
+                question_service.batch_create(question_to_create)
+                logger.info("Create question")
+            answers_for_this_question = question_with_answers["answers"]
+            created_question = question_service.get_list(
+                QuestionFilterEnitity(
+                    questions=[question], skill_ids=[created_skill[0].skill_id]
+                )
+            )
+
+            answers_to_create = []
+
+            for answer_with_right in answers_for_this_question:
+
+                answer = answer_with_right["answer"]
+                exist_answer = answer_service.get_list(
+                    AnswerFilterEnitity(
+                        answers=[answer], question_ids=[created_question[0].question_id]
                     )
                 )
-                answer_service.batch_create(answers_to_create)
+                if not exist_answer:
+                    try:
+                        right = answer_with_right["right"]
+                    except (KeyError):
+                        right = False
+
+                    answers_to_create.append(
+                        AnswerEntity(
+                            answer_id=None,
+                            answer=answer,
+                            right=right,
+                            question_id=created_question[0].question_id,
+                        )
+                    )
+                    answer_service.batch_create(answers_to_create)
+
+
+if __name__ == "__main__":
+    main()
