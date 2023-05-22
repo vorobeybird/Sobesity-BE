f20eeac57e35d5b5657e7b46c3b7effb469ef75d
devdeM1 <makd1232255@yandex.ru>
2023-05-22 11:15:46 +0300

    fix create answers + types + questions on tests

--- src/tests/intergration/views/test_define_knowledge.py
+++ src/tests/intergration/views/test_define_knowledge.py
@@ -27,7 +27,6 @@ def test_generate_questions__return_200(
 
 
 def test_scoring_full_right_return_100(client, auth_header, valid_scoring_100_body, type_repository, question_service, answer_service):
-    breakpoint()
     response = client.post("api/define_knowledge/scoring", json=valid_scoring_100_body, headers=auth_header,)
     assert response.status_code == HTTPStatus.OK
 
