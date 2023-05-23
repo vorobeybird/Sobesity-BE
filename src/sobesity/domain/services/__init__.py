62e70828a87acc4a98ea217d063de0d3b6b3f163
devdeM1 <makd1232255@yandex.ru>
2023-05-23 12:41:04 +0300

    create level service

--- src/sobesity/domain/services/__init__.py
+++ src/sobesity/domain/services/__init__.py
@@ -5,3 +5,4 @@ from .user import UserService
 from .type import TypeService
 from .generate_questions import QuestionGeneratorService
 from .scoring import ScoringService
+from .level import LevelService
---
+++ src/sobesity/domain/services/level.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from sobesity.domain.entities import LevelEntity, LevelFilterEnitity, LevelId
+from sobesity.domain.interfaces.repositories import ILevelRepository
+from sobesity.domain.interfaces.services import ILevelService
+
+
+class LevelService(ILevelService):
+    def __init__(self, level_repository: ILevelRepository) -> None:
+        self._level_repository = level_repository
+
+    def get_list(
+        self, level_filter: Optional[LevelFilterEnitity] = None
+    ) -> list[LevelEntity]:
+        return self._level_repository.get_list(level_filter)
+
+    def batch_create(self, levels: list[LevelEntity]) -> None:
+        return self._level_repository.batch_create(levels)
+
+    def update(
+        self, old_level: LevelEntity, update: LevelFilterEnitity
+    ) -> list[LevelId]:
+        return self._level_repository.update(old_level, update)
+
+    def delete(self, level_ids: list[LevelId]) -> None:
+        return self._level_repository.delete(level_ids)
