bb49bd2b55447c090bc6c1c9f22c54b94a011b7f
devdeM1 <makd1232255@yandex.ru>
2023-05-23 12:47:56 +0300

    create level view + exc

---
+++ src/sobesity/domain/exceptions/level.py
@@ -0,0 +1,14 @@
+
+from sobesity.domain.exceptions.base import DomainException
+
+
+class LevelNameUniqueViolation(DomainException):
+    def __init__(self) -> None:
+        self.message = "Level names must be unique"
+        super().__init__(self.message)
+
+
+class LevelNotExistViolation(Exception):
+    def __init__(self) -> None:
+        self.message = "Level not exist"
+        super().__init__(self.message)
--- src/sobesity/domain/views/__init__.py
+++ src/sobesity/domain/views/__init__.py
@@ -4,3 +4,4 @@ from .skill import skill_bp
 from .user import user_bp
 from .type import type_bp
 from .define_knowledge import define_knowledge_bp
+from .level import  level_bp
