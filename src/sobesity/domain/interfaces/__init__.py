8c68e68c5749542e3a1390b31e72c1ff81c4205c
devdeM1 <makd1232255@yandex.ru>
2023-05-23 12:30:49 +0300

    -

--- src/sobesity/domain/interfaces/__init__.py
+++ src/sobesity/domain/interfaces/__init__.py
@@ -1,3 +1,4 @@
 from .repositories.answer import IAnswerRepository
 from .repositories.question import IQuestionRepository
 from .repositories.skill import ISkillRepository
+from .repositories import ILevelRepository
